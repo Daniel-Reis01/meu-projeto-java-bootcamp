10e291e1eff8f7387c863f06ca1560c7591ed861
daniel almeida reis <daniel.7t@hotmail.com>
2022-10-11 00:42:37 -0300

    codando em java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("ola mundo agora sou java");
+    public static void main(String[] args)  {
+        String primeiroNome = "Daniel" ;
+        String segundoNome = "Almeida";
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome){ 
+        return "resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+
     }
 }
