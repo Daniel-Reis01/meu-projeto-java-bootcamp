538dc49372b99752c1b14ab3de162ee4de89efd3
daniel almeida reis <daniel.7t@hotmail.com>
2022-10-15 00:35:52 -0300

    atualizaçao java

---
+++ src/exercicio/execicioQuadrilatero.java
@@ -0,0 +1,14 @@
+package exercicio;
+
+public class execicioQuadrilatero {
+    public static void main(String[] args){
+
+        //Quadrilatero
+        System.out.println("Exercicio quadrilatero");
+        quadrilatereo.area(3);
+        quadrilatereo.area(5d,5d);
+        quadrilatereo.area(7,8,9);
+        quadrilatereo.area(5f,5f);
+    }
+
+}
