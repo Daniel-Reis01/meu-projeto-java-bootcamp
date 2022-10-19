83519c9554400686f5f1e9700725c4d703e9dcff
daniel almeida reis <daniel.7t@hotmail.com>
2022-10-19 00:32:06 -0300

    atualizaçao java

---
+++ src/programaçao/orientada/Carro.java
@@ -0,0 +1,40 @@
+package programaçao.orientada;
+
+public class Carro {
+
+    public Carro() {
+    }
+
+    public Carro(String string, String string2, int i) {
+    }
+
+    public void setCor(String string) {
+    }
+
+    public void setModelo(String string) {
+    }
+
+    public void setCapacidadeTanque(int i) {
+    }
+
+    public char[] getModelo() {
+        return null;
+    }
+
+    public char[] getCor() {
+        return null;
+    }
+
+    public char[] getCapacidadeTanque() {
+        return null;
+    }
+
+    public char[] totalValorTanque(double d) {
+        return null;
+    }
+
+    public char[] getCapacidade() {
+        return null;
+    }
+
+}
