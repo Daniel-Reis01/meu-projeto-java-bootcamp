9c5d2f444543962cd1c49a1fe29f8cfb1a564609
daniel almeida reis <daniel.7t@hotmail.com>
2022-10-19 18:48:08 -0300

    projeto java atualizacao

---
+++ src/exerciciofixacao/exercicioFixacao.java
@@ -0,0 +1,28 @@
+package exerciciofixacao;
+
+public class exercicioFixacao {
+    public static void main(String[] args) {
+        //O codigo ira imprimir todos os numeros de 150 a 300.
+
+        int i = 150;
+        while (i<=300){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+// ou 
+
+// class ImprimeSoma{
+// private static int soma;
+
+// public static void main(String[] args, char[] i) {
+    
+//      soma = 0;
+//      for (int i = 1; i <= 1000; i++){
+//         soma = soma + i;
+//      }
+//       System.out.println(i);
+//   }
+// }
